9c607d11d277fd28a7828a47a2db7f5d12ec2a5b
Krishna Paudel <krisnachhetri07@gmail.com>
2022-05-05 20:25:25 +1000

    backend done

---
+++ cfi_backend/Controller/criminalController.js
@@ -0,0 +1,69 @@
+const { error } = require("console");
+const asyncHandler = require("express-async-handler");
+
+const Criminal = require("../Model/criminalModel");
+
+//@desc get Criminal
+//@routes GET api/Criminal
+
+//@access Private
+const getCriminal = asyncHandler(async (req, res) => {
+  const criminals = await Criminal.find();
+  res.status(200).json({ message: criminals });
+  res.status(500).json({ message: error });
+});
+
+//@desc set Criminal
+//@routes POST api/Criminal
+//@access Private
+const setCriminal = asyncHandler(async (req, res) => {
+  const { name, Age, NoOffence, Found, Descriptors } = req.body;
+
+  if (!name || !Age || !NoOffence || !Descriptors) {
+    res.status(400);
+    throw new Error("Please add a text field!");
+  }
+  const criminal = await Criminal.create({
+    name,
+    Age,
+    NoOffence,
+    Found,
+    Descriptors,
+  });
+  res.status(200).json({ criminal });
+});
+
+//@desc update Criminal
+//@routes PUT api/Criminal/:id
+//@access Private
+const updateCriminal = asyncHandler(async (req, res) => {
+  const criminal = await Criminal.findById(req.params.id);
+  if (!criminal) {
+    res.status(400);
+    throw new Error("Criminal not found");
+  }
+  const updateCriminal = await Criminal.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+    }
+  );
+  res.status(200).json(updateCriminal);
+});
+
+//@desc Delete Criminal
+//@routes DELETE api/Criminal/:id
+//@access Private
+const deleteCriminal = asyncHandler(async (req, res) => {
+  const criminal = await Criminal.findById(req.params.id);
+  if (!criminal) {
+    res.status(400);
+    throw new Error("criminal not found");
+  }
+  await criminal.remove();
+
+  res.status(200).json({ id: req.params.id });
+});
+
+module.exports = { getCriminal, setCriminal, updateCriminal, deleteCriminal };
